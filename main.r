01e1144dd31f490a2e62683a5a078112e48caa09
SomeUnusualGames <106411602+SomeUnusualGames@users.noreply.github.com>
2023-01-05 10:00:10 -0300

    Fix wrong text color and update notes

--- main.r
+++ main.r
@@ -17,13 +17,14 @@ while (!window_should_close()) {
     game$game_over_timer <- 0.0
   }
   game <- update_game(game)
+
   begin_drawing()
   clear_background("black")
   if (!game$ball$game_over || game$game_over_timer > 0) {
     game <- draw_game(game)
   } else {
-    draw_text("GAME OVER!", 130, 330, 60, "black")
-    draw_text("Press R to reset", 130, 400, 40, "black")
+    draw_text("GAME OVER!", 130, 330, 60, "white")
+    draw_text("Press R to reset", 130, 400, 40, "white")
   }
   if (game$show_fps) {
     draw_fps(0, 0)
